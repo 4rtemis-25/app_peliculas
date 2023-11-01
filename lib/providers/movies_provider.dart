2fb0df32bd713e354cc0ac0e8bd2ae304ed528f3
Johan Hdez <hernandez.gomez29202@gmail.com>
2023-10-31 22:30:22 -0500

    correccion

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -1,10 +1,13 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class MoviesProvider extends ChangeNotifier {
 
   final String _apiKey = 'a21f1fc9ad3ccf99daa1f1fa6193110d';
-  final String _baseUrl = 'https://api.themoviedb.org';
+  final String _language = 'es-ES';
+  final String _baseUrl = 'api.themoviedb.org';
 
   MoviesProvider() {
     print('Movies Provider');
@@ -13,13 +16,12 @@ class MoviesProvider extends ChangeNotifier {
   }
 
   getOnDisplayMovies() async {
-    print('getOndisplayMovies');
-
-    var url = Uri.https(_baseUrl, '/3/movie/now_playing', {
-      'api_key': _apiKey,
+    var url = Uri.https( _baseUrl, '3/movie/now_playing', {
+      'api_key':_apiKey,
     });
 
     final response = await http.get(url);
-    print(response.body);
+    final Map<String, dynamic> decodedData = json.decode(response.body);
+    print(decodedData);
   }
 }
